9129f57768e02e4fe597c2e11857023323c3102c
elliotec <elliotecweb@gmail.com>
2014-02-15 17:07:58 -0800

    set routes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,17 @@
 class QuotesController < Rulers::Controller
   def a_quote
     "Theres nothing either good or bad " + 
-      "but thinking makes it so."
+      "but thinking makes it so." +
+      "\n<pre>\n#{env}\n</pre>"
+  end
+  def exception
+    begin
+      puts "I am before the exception."
+      raise "It's a bad one!"
+      puts "I am after the exception."
+    rescue
+      puts "I am rescued."
+    end
+    puts "I am after the begin block."
   end
 end
