728de19d44011e7c5a6a2f82558ffd6b23cb9829
elliotec <elliotecweb@gmail.com>
2014-02-17 10:06:43 -0800

    fix begin rescue block

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,14 +4,4 @@ class QuotesController < Rulers::Controller
       "but thinking makes it so." +
       "\n<pre>\n#{env}\n</pre>"
   end
-  def exception
-    begin
-      puts "I am before the exception."
-      raise "It's a bad one!"
-      puts "I am after the exception."
-    rescue
-      puts "I am rescued."
-    end
-    puts "I am after the begin block."
-  end
 end
