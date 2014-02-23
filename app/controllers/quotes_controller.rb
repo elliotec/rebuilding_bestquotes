533331759f7ca04cec077bd8c114f0fd42b42bc6
elliotec <elliotecweb@gmail.com>
2014-02-22 17:55:21 -0800

    add views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,5 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "Theres nothing either good or bad " + 
-      "but thinking makes it so." +
-      "\n<pre>\n#{env}\n</pre>"
+    render :a_quote, noun: :winking
   end
 end
